9fbf4e8d9cc17c6ea07de742282a82aee7cb18b8
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2017-12-29 17:05:01 -0700

    Step 8 - Use express.static

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 // The server folder must be in the root
 const express = require('express'); // similar to -> import express from 'express'
 const bodyParser = require('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const bc = require('./controllers/books_controller');  //bc - is just short for books_controller, it's just a variable name
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
-
+// app.use(cors());
+app.use(express.static('../public/build'));
 
 app.post('/api/books', bc.create);   // when someone posts it follows the books_controller instructions
 app.get('/api/books', bc.read);
